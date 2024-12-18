0c88cc7a683fdd2c7a0d75058e5ab7221775572f
Hari Pawann <pavantummalapalli253@gmail.com>
2024-12-18 19:50:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,10 +3,9 @@ provider "aws" {
 }
 
 resource "aws_instance" "backend" { #ubuntu.yaml NETADATA
-  ami                    = "ami-0fc5d935ebf8bc3bc"
+  ami                    = "ami-0166fe664262f664c"
   instance_type          = "t2.micro" 
-  key_name               = "ubuntu-key"
-  vpc_security_group_ids = ["sg-04cb5f715d70d6f35"]
+  key_name               = "jenkinsmaster"
   tags = {
     Name = "u21.local"
   }
@@ -23,10 +22,9 @@ EOF
 }
 
 resource "aws_instance" "frontend" { #amazon-playbook.yaml NGINX
-  ami                    = "ami-05c13eab67c5d8861"
+  ami                    = "ami-0166fe664262f664c"
   instance_type          = "t2.micro"
-  key_name               = "linux"
-  vpc_security_group_ids = ["sg-04cb5f715d70d6f35"]
+  key_name               = "jenkinsmaster"
   tags = {
     Name = "c8.local"
   }
